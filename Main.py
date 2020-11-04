011a9be64a333e3b3f7dffc2b1c0f90227a6a4a9
LightFox <gff1038m@gmail.com>
2020-11-04 16:00:10 +0300

    Эффективность сети приблизительно равна 97,4%
    И так, если при таком количестве эпох и коэффициенте обучения обучать нейронную сеть на настроящем MNIST (60к тренировочных данных), то можно добиться эффективности сети примрно 97%

--- Main.py
+++ Main.py
@@ -5,11 +5,7 @@ import Neural_network as Ai
 
 # Тут мы формируем список идеальных выходных значений сети из CSV-файла набора MNIST
 
-<<<<<<< Updated upstream
-training_data_file = open("mnist_dataset\mnist_train_100.csv", 'r')
-=======
 training_data_file = open("mnist_dataset\mnist_train_100.csv.csv", 'r')
->>>>>>> Stashed changes
 training_data_list = training_data_file.readlines()
 training_data_file.close()
 
@@ -19,27 +15,30 @@ hidden_nodes = 100
 output_nodes = 10
 
 # Коэффициент обучения
-learning_rate = 0.3
+learning_rate = 0.1
+
+# Количество эпох (тренировка на одном и том же датасете
+epochs = 5
 
 # Экзмемпляр нейрнонной сети
 NN = Ai.neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
 
 # Тренировка нейронной сети
+for epoch in range(epochs):
+    # Преобразовать все записи в тренировочном наборе данных
+    for record in training_data_list:
+        # Получить список значений, используя символы (',')  в качестве разделитилей
+        all_values = record.split(',')
+        # Данные представлены в диапазоне 0-255, для корректного использования сигмоиды нужен диапазон 0.01-1.0
+        # Поэтому данные мы корректируем значения в списке
+        inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
+        # Создать целевые выходные значения (все равны 0,01, за исключением желаемого маркерного значения, равного 0,99
+        targets = numpy.zeros(output_nodes) + 0.01
 
-# Преобразовать все записи в тренировочном наборе данных
-for record in training_data_list:
-    # Получить список значений, используя символы (',')  в качестве разделитилей
-    all_values = record.split(',')
-    # Данные представлены в диапазоне 0-255, для корректного использования сигмоиды нужен диапазон 0.01-1.0
-    # Поэтому данные мы корректируем значения в списке
-    inputs = (numpy.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01
-    # Создать целевые выходные значения (все равны 0,01, за исключением желаемого маркерного значения, равного 0,99
-    targets = numpy.zeros(output_nodes) + 0.01
-
-    # all_values[0] - целевое маркерное значение для данной записи
-    targets[int(all_values[0])] = 0.99
-    NN.train(inputs, targets)
-print("Trained")
+        # all_values[0] - целевое маркерное значение для данной записи
+        targets[int(all_values[0])] = 0.99
+        NN.train(inputs, targets)
+    print("Trained on ", epoch + 1, "epoch")
 
 # Тестирование нейронной сети
 
